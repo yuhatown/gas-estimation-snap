2be7a8f14b6509ea7d7a83476f85cca9291b9beb
yuhatown <yuhatown@gmail.com>
2023-05-23 19:08:33 +0900

    tutorial

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,31 +1,25 @@
 import { OnRpcRequestHandler } from '@metamask/snaps-types';
 import { panel, text } from '@metamask/snaps-ui';
 
-/**
- * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
- *
- * @param args - The request handler args as object.
- * @param args.origin - The origin of the request, e.g., the website that
- * invoked the snap.
- * @param args.request - A validated JSON-RPC request object.
- * @returns The result of `snap_dialog`.
- * @throws If the request method is not valid for this snap.
- */
+async function getFees() {
+  const response = await fetch('https://beaconcha.in/api/v1/execution/gasnow'); 
+  return response.text();
+}
+
 export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
   switch (request.method) {
     case 'hello':
-      return snap.request({
-        method: 'snap_dialog',
-        params: {
-          type: 'confirmation',
-          content: panel([
-            text(`Hello, **${origin}**!`),
-            text('This custom confirmation is just for display purposes.'),
-            text(
-              'But you can edit the snap source code to make it do something, if you want to!',
-            ),
-          ]),
-        },
+      return getFees().then(fees => {
+        return snap.request({
+          method: 'snap_dialog',
+          params: {
+            type: 'alert',
+            content: panel([
+              text(`Hello, **${origin}**!`),
+              text(`Current gas fee estimates: ${fees}`),
+            ]),
+          },
+        });
       });
     default:
       throw new Error('Method not found.');
